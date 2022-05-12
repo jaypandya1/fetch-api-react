5a6851ea8ca787f5a3b2502d02199c4fbc29a9b4
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2022-05-12 00:27:48 -0400

    fetched data using fetch()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  //React State
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  //We are using useEffect Hook To Fetch the data
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`This is HTTP Error ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-     
+      <h1>Api Posts</h1>
+      {loading && <div>Please Wait .... </div>}
+      {error && <div>{`There is a problem fetching the data - ${error}`}</div>}
+      <ul>
+        {data &&
+  
+          data.map((data) => {
+            return <li key={data.id}>
+              <h3>{data.title}</h3>
+            </li>
+          })}
+      </ul>
     </div>
   );
 }
